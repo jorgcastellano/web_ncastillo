985e65525a1113e1c0ac6df78c22b774d86c48e6
Jorge Castellano <jorgecm14@gmail.com>
2017-07-10 07:53:04 -0400

    Maqueta principal completa

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,8 @@ define('DB_NAME', 'nils_castillo');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '65445625');
+//define('DB_PASSWORD', '65445625');
+define('DB_PASSWORD', 'jorgejac');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
